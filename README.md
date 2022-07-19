514fe469db6e688ca1c91ffba4b69be478666e76
lilasteroid <andrey_bobrov@outlook.de>
2022-07-19 15:41:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-jxwskj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-jxwskj)
---
+++ index.ts
@@ -0,0 +1,20 @@
+import { TodoItem } from './todo/todoItem';
+import { TodoCollection } from './todo/todoCollection';
+
+let todos = [
+  new TodoItem(1, 'Buy Flowers'),
+  new TodoItem(2, 'Get Shoes'),
+  new TodoItem(3, 'Collect Tickets'),
+  new TodoItem(4, 'Call Joe', true),
+];
+
+let collection = new TodoCollection('Adam', todos);
+
+console.clear();
+console.log(`${collection.userName}'s Todo List`);
+
+let newId = collection.addTodo('Go for run');
+let todoItem = collection.getTodoById(newId);
+todoItem.printDetails();
+
+//console.log(JSON.stringify(todoItem));
